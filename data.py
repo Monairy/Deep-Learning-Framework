9cec5f7c3ecc453b9dede204ef4966bc4c59ef73
ahmedalaa11 <47926165+ahmedalaa11@users.noreply.github.com>
2021-01-19 20:47:35 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import numpy as np
 import h5py
 import scipy
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
 
 def batch_normlize(x):
     eps=0.00001
@@ -41,9 +44,39 @@ def generate_dummy_data(Type,Visualize=0):
         train_Y = train_Y.reshape((1, train_Y.shape[0]))
         test_X = test_X.T
         test_Y = test_Y.reshape((1, test_Y.shape[0]))
-    return train_X, train_Y, test_X, test_Y
+        return train_X, train_Y, test_X, test_Y
+    
+    
+    if (Type == 'plannar'):
+        np.random.seed(1)
+        m = 400  # number of examples
+        N = int(m / 2)  # number of points per class
+        D = 2  # dimensionality
+        X = np.zeros((m, D))  # data matrix where each row is a single example
+        Y = np.zeros((m, 1), dtype='uint8')  # labels vector (0 for red, 1 for blue)
+        a = 4  # maximum ray of the flower
 
+        for j in range(2):
+            ix = range(N * j, N * (j + 1))
+            t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+            r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+            X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+            Y[ix] = j
 
+        X = X.T
+        Y = Y.T
+
+        return X, Y
+    
+    if(Type == 'extra'):
+        N = 200
+        noisy_circles = sklearn.datasets.make_circles(n_samples=N, factor=.5, noise=.3)
+        noisy_moons = sklearn.datasets.make_moons(n_samples=N, noise=.2)
+        blobs = sklearn.datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=6)
+        gaussian_quantiles = sklearn.datasets.make_gaussian_quantiles(mean=None, cov=0.5, n_samples=N, n_features=2,n_classes=2, shuffle=True, random_state=None)
+        no_structure = np.random.rand(N, 2), np.random.rand(N, 2)
+
+        return noisy_circles, noisy_moons, blobs, gaussian_quantiles, no_structure
 
 X = np.array(
            [
