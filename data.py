2e3188f0ef9532a9c9dc3ab032e727c27f099886
Ahmed EL-Monairy <42189296+Monairy@users.noreply.github.com>
2021-01-19 15:24:09 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def batch_normlize(x):
+    eps=0.00001
+    
+    mean = x.mean(axis=0)
+    variance = x.var(axis=0)
+    
+    xnormalized = (x - mean) / np.sqrt(variance + eps)
+ 
+    return xnormalized
+
+
+def generate_dummy_data():
+    pass
+
+
+
+X = np.array(
+           [
+                      [1,1],
+                      [2,2],
+                      ]
+           )
+
+y=batch_normlize(X)
+print(y)
