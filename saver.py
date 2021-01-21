5238cc70c9860d99532d93ac0939704b22a757cc
ahmedalaa11 <47926165+ahmedalaa11@users.noreply.github.com>
2021-01-21 14:53:13 +0200

    Update saver.py

--- saver.py
+++ saver.py
@@ -2,8 +2,27 @@ import pickle
 
 
 def save(filename,model):
+     """ This function saves the model ( all of it's parameters, weights, losses ,... ) in an external file 
+
+    Parameters:
+    filename (path?!): Path of file to be saved in.
+    model : Object "model" required to be saved.
+
+    Returns:
+    None
+
+   """
     pickle.dump(model, open(filename, 'wb'))
 
 def retrive(filename):
+     """ This function loads a saved model ( all of it's parameters, weights, losses, ...) from external file
+
+    Parameters:
+    filename (path?!): Path of file to be retrived
+
+    Returns:
+    model : ?!
+
+   """
     return pickle.load(open(filename, 'rb'))
 
