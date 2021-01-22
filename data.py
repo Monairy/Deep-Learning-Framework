49e2cbd69fe4a9b9956eb6d738cbfaf2a92f0d1a
Ahmed EL-Monairy <42189296+Monairy@users.noreply.github.com>
2021-01-22 12:57:10 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def batch_normlize(x):
@@ -61,3 +62,36 @@ y=batch_normlize(X)
 print(y)
 
 
+class visualize:
+
+ points_y=[]
+ points_x=[]
+     
+ fig=plt.figure(figsize=[10,8])
+ graph=fig.subplots()
+
+ def __init__(self,title,y_label,x_label):
+     
+            self.graph.set_ylabel(y_label)
+            self.graph.set_xlabel(x_label)
+            self.graph.set_title(title)
+            
+ def addpoint_y(self,pointy):
+     
+         self.points_y.append(pointy)
+         if (len(self.points_x) ==0):
+            self.points_x=[1]
+               
+         else:
+            self.points_x.append(len(self.points_x)+1)
+               
+         self.graph.plot(self.points_x,self.points_y,'bo')
+         self.fig.show()
+         plt.pause(0.0001)
+
+            
+    
+a = visualize("cost function","cost","iterations")
+
+for i in range(50):
+           a.addpoint_y(i)
